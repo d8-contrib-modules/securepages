3d60397a37bb8b312a3d33fee0b5277d84c2b6a4
Jose Moreira <jose.moreira@acquia.com>
2015-09-10 14:20:33 -0400

    Removing destination query when redirecting to https

--- src/EventSubscriber/SecurePagesEventSubscriber.php
+++ src/EventSubscriber/SecurePagesEventSubscriber.php
@@ -52,14 +52,16 @@ class SecurePagesEventSubscriber implements EventSubscriberInterface {
 
       $securepagesservice = \Drupal::service('securepages.securepagesservice');
       $redirect = $securepagesservice->securePagesRedirect();
+      $request = $event->getRequest();
 
         if(is_null($redirect)) {
 
         }elseif($redirect == TRUE) {
-          $url = Url::fromUri($event->getRequest()->getUri(), array('absolute' => TRUE, 'https' => TRUE))->toString();
+          $request->query->remove('destination');
+          $url = Url::fromUri($request->getUri(), array('absolute' => TRUE, 'https' => TRUE))->toString();
           $event->setResponse(new RedirectResponse($url, 302));
         }elseif($redirect == FALSE){
-          $url = Url::fromUri($event->getRequest()->getUri(), array('absolute' => TRUE, 'https' => FALSE))->toString();
+          $url = Url::fromUri($request->getUri(), array('absolute' => TRUE, 'https' => FALSE))->toString();
           $event->setResponse(new RedirectResponse($url, 302));
         }
 
